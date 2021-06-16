e29987f28010dbf6a340a92ea2c7cec8e0638537
rwfarish <rwfarish@gmail.com>
2021-06-16 18:22:07 -0400

    favicon and responsiveness

--- src/views/MyPage/components/Footer/Footer.js
+++ src/views/MyPage/components/Footer/Footer.js
@@ -3,33 +3,43 @@ import React from 'react';
 import styles from './Footer.module.css';
 import PropTypes from 'prop-types';
 import { IconAlternate } from 'components/molecules';
-import { Button } from '@material-ui/core';
+import { Button, Grid, Paper } from '@material-ui/core';
 
-const Footer = ({ noBorder }) => (
-  <footer>
-    <div className={noBorder ? styles.containerNoBorder : styles.container}>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com/in/bo-farish/"
-            >
-              <i class="fab fa-linkedin"></i>
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/rwfarish"
-            >
-              <i class="fab fa-github fa-lg"></i>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </footer>
-);
+const Footer = () => {
+  return(
+    <Grid container spacing={1}>
+      <Grid item xs={12}> 
+        <Paper> Footer Goes Here </Paper>
+      </Grid>
+    </Grid>
+  )
+}
+
+// const Footer = ({ noBorder }) => (
+//   <footer>
+//     <div className={noBorder ? styles.containerNoBorder : styles.container}>
+//       <nav className={styles.nav}>
+//         <ul>
+//           <li>
+//             <a
+//               target="_blank"
+//               rel="noopener noreferrer"
+//               href="https://www.linkedin.com/in/bo-farish/"
+//             >
+//               <i class="fab fa-linkedin"></i>
+//             </a>
+//             <a
+//               target="_blank"
+//               rel="noopener noreferrer"
+//               href="https://github.com/rwfarish"
+//             >
+//               <i class="fab fa-github fa-lg"></i>
+//             </a>
+//           </li>
+//         </ul>
+//       </nav>
+//     </div>
+//   </footer>
+// );
 
 export default Footer;
